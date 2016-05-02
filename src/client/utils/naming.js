05bc1cb8ca0a4025859771566148045bcc0ea3e9
Daniel Edler <edler.dev@gmail.com>
2016-05-02 04:53:27 +0200

    Fix missing import for cli tool

--- src/client/utils/naming.js
+++ src/client/utils/naming.js
@@ -1,7 +1,8 @@
 // import sentenceCase from 'sentence-case';
+import _ from 'lodash';
 
 export function normalizeSpeciesName(speciesName) {
     if (!speciesName)
         return speciesName;
-    return _.upperFirst(speciesName.replace('_', ' '));
+    return _.upperFirst(speciesName.replace(/_/g, ' '));
 }
