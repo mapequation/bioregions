a7b5ee8ca9b7f1febcd7383e493fc926698eed20
Daniel Edler <edler.dev@gmail.com>
2019-06-02 22:42:49 +0200

    Fix use NODE_ENV production in production build

--- webpack/production.js
+++ webpack/production.js
@@ -3,7 +3,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
 var devFlagPlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false')),
+  'process.env': {
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }
 });
 
 module.exports = {
