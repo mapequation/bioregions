1204116e9e76aab1d6c31a309a15c3b7e95606e6
Anton Eriksson <anton.eriksson@umu.se>
2021-10-19 15:31:04 +0200

    wip: Extract TangleInputs

--- src/components/ControlPanel/Resolution.tsx
+++ src/components/ControlPanel/Resolution.tsx
@@ -1,7 +1,28 @@
 import { observer } from 'mobx-react';
 import { Box, VStack, Flex, Spacer, Text } from '@chakra-ui/react';
 import { useStore } from '../../store';
-import TangleInput from '../TangleInput';
+import TangleInput, { TangleInputProps } from '../TangleInput';
+
+type TangleInputsProps = {
+  label: string;
+  minProps: TangleInputProps;
+  maxProps: TangleInputProps;
+}
+
+const TangleInputs = ({ label, minProps, maxProps }: TangleInputsProps) => {
+  return (
+    <>
+      <Box>{label}</Box>
+      <Spacer />
+      <Box>
+        <Text fontSize="xs" textTransform="uppercase">
+          Min: <TangleInput {...minProps} />{' '}
+          Max: <TangleInput {...maxProps} />
+        </Text>
+      </Box>
+    </>
+  )
+}
 
 
 export default observer(function Resolution() {
@@ -14,67 +35,49 @@ export default observer(function Resolution() {
   return (
     <VStack align="flex-start">
       <Flex w="100%">
-        <Box>Cell size</Box>
-        <Spacer />
-        <Box>
-          <Text fontSize="xs" textTransform="uppercase">
-            Min:{' '}
-            <TangleInput
-              suffix="˚"
-              value={binner.minNodeSizeLog2}
-              min={-3}
-              max={binner.maxNodeSizeLog2}
-              format={formatBinSize}
-              step={1}
-              speed={0.1}
-              onChange={(value) => binner.setMinNodeSizeLog2(value)}
-            />{' '}
-            Max:{' '}
-            <TangleInput
-              suffix="˚"
-              value={binner.maxNodeSizeLog2}
-              min={binner.minNodeSizeLog2}
-              max={6}
-              format={formatBinSize}
-              speed={0.1}
-              onChange={(value) => {
-                binner.setMaxNodeSizeLog2(value);
-              }}
-            />
-          </Text>
-        </Box>
+        <TangleInputs
+          label="Cell size"
+          minProps={{
+            suffix: '˚',
+            value: binner.minNodeSizeLog2,
+            min: -3,
+            max: binner.maxNodeSizeLog2,
+            format: formatBinSize,
+            step: 1,
+            speed: 0.1,
+            onChange: (value) => binner.setMinNodeSizeLog2(value),
+          }}
+          maxProps={{
+            suffix: '˚',
+            value: binner.maxNodeSizeLog2,
+            min: binner.minNodeSizeLog2,
+            max: 6,
+            format: formatBinSize,
+            speed: 0.1,
+            onChange: (value) => binner.setMaxNodeSizeLog2(value),
+          }}
+        />
       </Flex>
       <Flex w="100%" alignItems="flex-end">
-        <Box>Cell capacity</Box>
-        <Spacer />
-        <Box>
-          <Text fontSize="xs" textTransform="uppercase">
-            Min:{' '}
-            <TangleInput
-              value={binner.lowerThreshold}
-              min={0}
-              max={binner.nodeCapacity}
-              logScale
-              speed={0.2}
-              onChange={(value) => {
-                console.log('binner change min:', value);
-                binner.setLowerThreshold(value);
-              }}
-            />{' '}
-            Max:{' '}
-            <TangleInput
-              value={binner.nodeCapacity}
-              min={binner.lowerThreshold}
-              max={1000}
-              logScale
-              speed={0.2}
-              onChange={(value) => {
-                console.log('binner change max:', value);
-                binner.setNodeCapacity(value);
-              }}
-            />
-          </Text>
-        </Box>
+        <TangleInputs
+          label="Cell capacity"
+          minProps={{
+            value: binner.lowerThreshold,
+            min: 0,
+            max: binner.nodeCapacity,
+            logScale: true,
+            speed: 0.2,
+            onChange: (value) => binner.setLowerThreshold(value),
+          }}
+          maxProps={{
+            value: binner.nodeCapacity,
+            min: binner.lowerThreshold,
+            max: 1000,
+            logScale: true,
+            speed: 0.2,
+            onChange: (value) => binner.setNodeCapacity(value),
+          }}
+        />
       </Flex>
     </VStack>
   );
