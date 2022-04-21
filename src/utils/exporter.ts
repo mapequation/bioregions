9ced38fdb58aca17262d964ded58bf5b26bd56f0
Daniel Edler <edler.dev@gmail.com>
2022-04-21 10:41:05 +0200

    chore: expose type in save string function

--- src/utils/exporter.ts
+++ src/utils/exporter.ts
@@ -1,6 +1,10 @@
 import { saveAs } from 'file-saver';
 
-export function saveString(filename: string, content: string) {
-  const blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+export function saveString(
+  filename: string,
+  content: string,
+  type = 'text/plain;charset=utf-8',
+) {
+  const blob = new Blob([content], { type });
   saveAs(blob, filename);
 }
