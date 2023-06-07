12af8ff7a1bb0fb5ac57a96161455f01f3c2fb0c
Daniel Edler <edler.dev@gmail.com>
2023-06-07 22:02:45 +0200

    fix: Shapefile binning resolution, fixes #55

--- src/client/workers/DataWorker.js
+++ src/client/workers/DataWorker.js
@@ -490,7 +490,7 @@ function parseGeoJSON(nameField) {
 function shapeToPoints() {
   state.features = [];
   // Divide by 2 to reduce edge cases where species are not included
-  const minNodeSize = getNodeSize(state.binning.minNodeSizeLog2); // 2
+  const minNodeSize = getNodeSize(state.binning.minNodeSizeLog2) / 2;
   const halfMinNodeSize = minNodeSize / 2;
   // const resolution = minNodeSize / 8;
   const tolerance = minNodeSize / 8;
