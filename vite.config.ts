36fb8c60d03442817cd326213013a2950bd30419
Daniel Edler <edler.dev@gmail.com>
2025-12-18 02:55:01 +0100

    chore(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-  }
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
