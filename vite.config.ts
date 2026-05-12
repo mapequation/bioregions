8db6609188e1d277a3dd39eb4fc745449dc4cd72
Daniel Edler <edler.dev@gmail.com>
2026-05-12 13:37:20 +0200

    build: Fix platform-dependent lock file sync issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bioregions2',
+  base: '/bioregions',
   plugins: [react(), tsconfigPaths()],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
